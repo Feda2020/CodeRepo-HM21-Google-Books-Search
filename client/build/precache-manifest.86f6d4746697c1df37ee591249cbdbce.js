7dd0466aa324ba3a2716456b1cb083f7f967be91
Feda Zidan <fido311@gmail.com>
2020-12-16 23:18:45 -0600

    Deploying to Heroku

---
+++ client/build/precache-manifest.86f6d4746697c1df37ee591249cbdbce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9169536ebbf0ca15e4ef270d996551f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c0c26c6a6dedde154cb",
+    "url": "/static/css/main.c33bdaf5.chunk.css"
+  },
+  {
+    "revision": "e0e789e610e75e6e018c",
+    "url": "/static/js/2.c6ace617.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c6ace617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0c26c6a6dedde154cb",
+    "url": "/static/js/main.bc8a3c3b.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "a5849099fe159700722c2d402a5a809f",
+    "url": "/static/media/books.a5849099.jpg"
+  }
+]);
